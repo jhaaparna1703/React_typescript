004790afdc92dcf5b40ea96379ee007fba55b3f0
Aparna Jha <aparna.jha@stackinfinite.com>
2023-01-12 19:10:18 +0530

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import InputField from "./components/InputField";
-import TodoList from './components/TodoList';
-import { Todo } from "./modal"
 
-const App: React.FC = () => {
+//components
+import header from './components/header';
 
-  const [todo, setTodo] = useState<string>("")
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  const handleAdd = (e: React.FormEvent) => {
-    e.preventDefault();
-
-    if (todo) {
-      setTodos([...todos, { id: Date.now(), todo, isDone: false }])
-      setTodo("");
-    }
-  };
-
-  console.log(todos);
-
+function App() {
   return (
     <div className="App">
-      <span className="heading ">Taskify</span>
-      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
-      <TodoList todos={todos} setTodos={setTodos} />
-      {/* {todos.map((t) => (
-        <li>{t.todo}</li>
-      ))} */}
+      <header/>
     </div>
   );
 }
