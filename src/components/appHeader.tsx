8da52dd1a20185b4edf338777195436d5e54c63e
Aparna Jha <aparna.jha@stackinfinite.com>
2023-01-16 16:55:13 +0530

    3rd commit

--- src/components/appHeader.tsx
+++ src/components/appHeader.tsx
@@ -1,34 +1,31 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { FaTruck } from "react-icons/fa";
-import {BiSearchAlt2} from "react-icons/bi"
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { FaTruck,FaCartPlus } from "react-icons/fa";
+import { BiSearchAlt2 } from "react-icons/bi";
 import "./components.css";
 
 export const Header = () => {
   return (
-    <Navbar className="navbar" variant="dark">
-      <FaTruck className="FaTruck" />
-      <Container>
-        <Navbar.Brand className="apnastore" href="#home">
-          Apna Store
-        </Navbar.Brand>
-        <div className="input-group">
-          <input
-            type="search"
-            className="searchbar"
-            placeholder="Search for products"
-            aria-label="Search"
-            aria-describedby="search-addon"
+    <Navbar className="navbar" expand="lg">
+      <Container fluid>
+        <FaTruck className="FaTruck" />
+        <Navbar.Brand className="apnastore">Apna Store</Navbar.Brand>
 
-            
+        <Form className="d-flex">
+          <Form.Control
+            type="search"
+            placeholder="Search for products"
+            className="searchbox"
+            aria-label="Search"
           />
-          <span className="search_icon">
-          <BiSearchAlt2 className="BisearchAlt2"/>
-          </span>
           
-        </div>
+            <BiSearchAlt2 className="BiSearchAlt2"/>
+          
+        </Form>
+        <FaCartPlus className="FaCartPlus"/>
       </Container>
     </Navbar>
   );
